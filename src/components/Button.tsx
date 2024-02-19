c3dd81dbcf15c0b53841912bc73484c1a2acd183
“Sam <smans123@gmail.com>
2024-02-19 13:11:37 -0500

    Adding way to set color from parent with default

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 interface Props {
   children: string;
+  color?: string; // Optional property w/ default value
   onClick: () => void;
 }
 
-const Button = ({ children, onClick }: Props) => {
+const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
-    <button className="btn btn-primary" onClick={onClick}>
+    <button className={"btn btn-" + color} onClick={onClick}>
       {children}
     </button>
   );
