7c7cd38e9184e0df8b38ffbd8d6e5ff30bb98405
“Sam <smans123@gmail.com>
2024-03-04 19:55:53 -0500

    Adding nav styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,25 @@ import { Link, NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
-      <Link to={`/`}>Main</Link>
-      <NavLink to={`/about`}>About</NavLink>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <div id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to={`/`}>
+                Main
+              </Link>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={`/about`}>
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* <Link to={`/`}>Main</Link>
+      <NavLink to={`/about`}>About</NavLink> */}
     </nav>
   );
 }
