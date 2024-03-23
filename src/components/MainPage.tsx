f8e7229a553d41fd6964dbd3ed7bb6430c9fcf83
“Sam <smans123@gmail.com>
2024-03-23 17:23:23 -0400

    Setting up logic to make an example API call

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,7 @@
 import ListGroup from "./ListGroup";
 import Alert from "./Alert";
 import Button from "./Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 let wards = [
   "Shibuya (渋谷区)",
@@ -12,6 +12,34 @@ let wards = [
 ];
 
 function MainPage() {
+  const [data, setData] = useState({} as any);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null as any);
+
+  // What is the purpose of useEffect?
+  // It has to be used in the body of a function component to work.
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("calling...");
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts/1"
+        );
+        const json = await response.json();
+        console.log("json response", json);
+        setData(json);
+        console.log("stringified res", JSON.stringify(data, null, 2));
+      } catch (err: any) {
+        console.log("err", err);
+        setError(err);
+      } finally {
+        console.log("call complete!");
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   const handleSelectItem = (item: string) => {
     console.log("item", item);
   };
@@ -20,6 +48,12 @@ function MainPage() {
     setAlert(false);
   };
 
+  // Return markup based on state
+  // Loading, display placeholder
+  if (loading) return <div>Loading...</div>;
+  // Error, display error message
+  if (error) return <div>Error: {error.message}</div>;
+  // Complete, Render full template
   return (
     <div>
       <h2>Main Page</h2>
@@ -28,6 +62,8 @@ function MainPage() {
           <span className="fs-3">Hello World!</span>
         </Alert>
       )}
+
+      <p>{data.body}</p>
 
       <div>
         <Button disabled={showAlert} onClick={() => setAlert(true)}>
